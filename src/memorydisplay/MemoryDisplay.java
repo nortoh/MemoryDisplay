2a8566104782189eb0caba16c4c04dd72260b6aa
Christian <Christian@Christian-PC>
2020-02-21 16:05:46 -0500

    Housekeeping

--- src/memorydisplay/MemoryDisplay.java
+++ src/memorydisplay/MemoryDisplay.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package memorydisplay;
 
 import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
- *
- * @author Christian
+ * @version 1.0
+ * @author Christian Horton
  */
 public class MemoryDisplay {
 
     /**
-     * @param args the command line arguments
+     * Main method
+     * @param args 
      */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
